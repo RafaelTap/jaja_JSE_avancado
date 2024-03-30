47fe54fff10427277e9dbf5eb7b0cfe59d782735
RafaelTaparica <rafael_taparica@outlook.com>
2024-03-29 23:10:10 -0300

    adicao de novo exercicio: Applets

---
+++ src/edu/Applets/Introducao.java
@@ -0,0 +1,12 @@
+package edu.Applets;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Introducao extends Applet {
+
+	public void paint(Graphics g) {
+		g.drawString("exemplo applets", 40, 50);
+	}
+
+}
